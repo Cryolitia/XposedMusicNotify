be5507fac1d51a15143461da3984de86280f8121
singleNeuron <liziyuan123456@yeah.net>
2020-04-03 19:32:06 +0200

    add SELinux detect for debugTool

--- app/src/main/java/cn/nexus6p/QQMusicNotify/Hook/mesingleneuronoriginalmusicnotification_debugtool.kt
+++ app/src/main/java/cn/nexus6p/QQMusicNotify/Hook/mesingleneuronoriginalmusicnotification_debugtool.kt
@@ -6,6 +6,8 @@ import cn.nexus6p.QQMusicNotify.Base.BasicInit
 import cn.nexus6p.QQMusicNotify.BuildConfig
 import cn.nexus6p.QQMusicNotify.ContentProvider
 import cn.nexus6p.QQMusicNotify.SharedPreferences.ContentProviderPreference
+import cn.nexus6p.QQMusicNotify.Utils.GeneralUtils
+import de.robv.android.xposed.SELinuxHelper
 import de.robv.android.xposed.XC_MethodReplacement
 import de.robv.android.xposed.XposedHelpers
 import org.jetbrains.anko.toast
@@ -22,9 +24,13 @@ class mesingleneuronoriginalmusicnotification_debugtool : BasicInit() {
                 mParam = param
                 Log.d("nexus", "已进入调试器")
                 basicParam.context!!.toast("Xposed已经注入")
-                print("原生音乐通知已找到：\n\n\n\n版本名: " + BuildConfig.VERSION_NAME + "\n\n\n\n版本号：" + BuildConfig.VERSION_CODE)
+                val selinux = if (SELinuxHelper.isSELinuxEnabled()) if (SELinuxHelper.isSELinuxEnforced()) "Enforcing" else "Permissive" else "Disabled"
+                print("SeLinux: $selinux")
+                print("原生音乐通知已找到：\n版本名: " + BuildConfig.VERSION_NAME + "\n版本号：" + BuildConfig.VERSION_CODE)
                 print("Context: " + basicParam.context.toString())
+                print("ModuleContext: " + GeneralUtils.getModuleContext(basicParam.context))
                 print("ClassLoader: $classLoader")
+                print("ModuleClassLoader: " + GeneralUtils.getModuleContext(basicParam.context).classLoader)
                 val jsonString: String = ContentProviderPreference(ContentProvider.CONTENT_PROVIDER_JSON, "me.singleneuron.originalmusicnotification_debugtool", basicParam.context!!).getJSONString()
                 print("JSONString: $jsonString")
                 val settingJsonString: String = ContentProviderPreference(ContentProvider.CONTENT_PROVIDER_PREFERENCE, null, basicParam.context!!).getJSONString()
