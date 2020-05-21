a701da52c2f73cac17b05baa2c7ede45d69480b4
singleNeuron <liziyuan123456@yeah.net>
2020-05-22 01:39:04 +0200

    sync removewhiteframe with 20200521-beta version

--- app/src/main/java/cn/nexus6p/removewhitenotificationforbugme/main.java
+++ app/src/main/java/cn/nexus6p/removewhitenotificationforbugme/main.java
@@ -1,22 +1,19 @@
 package cn.nexus6p.removewhitenotificationforbugme;
 
-import android.app.Application;
-import android.content.Context;
 import android.content.res.Resources;
 
 import java.util.ArrayList;
 
+import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-public class main {
+public class main implements IXposedHookInitPackageResources, IXposedHookLoadPackage {
 
-    private final static String minHeight = "notification_min_height_legacy";
-    private final static int MINHEIGHT = 133;
     private static ArrayList<String> nameStrings = new ArrayList<>();
-
     static {
         nameStrings.add("notification_background_custom_padding_left");
         nameStrings.add("notification_background_custom_padding_top");
@@ -26,53 +23,11 @@ public class main {
         nameStrings.add("notification_custom_view_margin_start");
         nameStrings.add("notification_custom_view_corner_radius");
         nameStrings.add("notification_row_extra_padding");
+        nameStrings.add("notification_custom_view_margin");
     }
 
-    public static void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
-        if (!lpparam.packageName.equals("com.android.systemui")) return;
-        final ClassLoader classLoader = lpparam.classLoader;
-        XposedHelpers.findAndHookMethod(Application.class.getName(), classLoader, "attach", Context.class, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Context context = (Context) param.args[0];
-                /*final int leftID = context.getResources().getIdentifier("notification_background_custom_padding_left","dimen","com.android.systemui");
-                final int topID = context.getResources().getIdentifier("notification_background_custom_padding_top","dimen","com.android.systemui");
-                final int rightID = context.getResources().getIdentifier("notification_background_custom_padding_right","dimen","com.android.systemui");
-                final int bottomID = context.getResources().getIdentifier("notification_background_custom_padding_bottom","dimen","com.android.systemui");*/
-                final ArrayList<Integer> IDs = new ArrayList<>();
-                for (String nameString : nameStrings) {
-                    try {
-                        IDs.add(context.getResources().getIdentifier(nameString, "dimen", "com.android.systemui"));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-                XposedHelpers.findAndHookMethod(Resources.class, "getDimensionPixelSize", int.class, new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        int resID = (int) param.args[0];
-                        for (Integer ID : IDs) {
-                            if (resID == ID) {
-                                param.setResult(0);
-                                return;
-                            }
-                        }
-                    }
-                });
-            }
-        });
-    }
-
-    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) {
-        /*if (!resparam.packageName.equals("com.android.systemui")) return;
-        final int leftID = resparam.res.getIdentifier("notification_background_custom_padding_left","dimen","com.android.systemui");
-        final int topID = resparam.res.getIdentifier("notification_background_custom_padding_top","dimen","com.android.systemui");
-        final int rightID = resparam.res.getIdentifier("notification_background_custom_padding_right","dimen","com.android.systemui");
-        final int bottomID = resparam.res.getIdentifier("notification_background_custom_padding_bottom","dimen","com.android.systemui");
-        resparam.res.setReplacement(leftID,0);
-        resparam.res.setReplacement(topID,0);
-        resparam.res.setReplacement(rightID,0);
-        resparam.res.setReplacement(bottomID,0);*/
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
         for (String nameString : nameStrings) {
             try {
                 final int ID = resparam.res.getIdentifier(nameString, "dimen", "com.android.systemui");
@@ -81,5 +36,32 @@ public class main {
                 e.printStackTrace();
             }
         }
+    }
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.android.systemui")) return;
+        final ClassLoader classLoader = lpparam.classLoader;
+
+                XposedHelpers.findAndHookMethod(Resources.class, "getDimensionPixelSize", int.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        final ArrayList<Integer> IDs = new ArrayList<>();
+                        for (String nameString : nameStrings) {
+                            try {
+                                IDs.add(((Resources)param.thisObject).getIdentifier(nameString, "dimen", "com.android.systemui"));
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        int resID = (int) param.args[0];
+                        for (Integer ID : IDs) {
+                            if (resID == ID) {
+                                param.setResult(0);
+                                return;
+                            }
+                        }
+                    }
+        });
     }
 }
